0890e396358430f2e4f37f810ba45ff9189c0cdd
lagunasdaniel001 <lagunasdaniel001@gmail.com>
2023-06-26 16:26:27 -0500

    Pagina para cambiar el tema

--- pages/theme-changer.tsx
+++ pages/theme-changer.tsx
@@ -1,9 +1,31 @@
 import { Layout } from "@/components/layout"
+import {Card, CardHeader, CardContent, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio} from '@mui/material'
+import { ChangeEvent, useState } from "react"
+
 
 const ThemeChangerPage = () => {
+    const [currentTheme, setCurrentTheme] = useState('light')
+    const onThemeChange = ( event:ChangeEvent<HTMLInputElement>) => {
+        setCurrentTheme( event.target.value)
+    }
   return (
     <Layout>
-        <h1>ThemeChangerPage</h1>
+        <Card>
+            <CardHeader title='Cambiar Modo'/>
+            <CardContent>
+                <FormControl>
+                    <FormLabel>Tema</FormLabel>
+                    <RadioGroup
+                        value={currentTheme}
+                        onChange={onThemeChange}
+                    >
+                        <FormControlLabel value={'light'} control={<Radio/>} label='Light'/>
+                        <FormControlLabel value={'dark'} control={<Radio/>} label='Dark'/>
+                        <FormControlLabel value={'custom'} control={<Radio/>} label='Custom'/>
+                    </RadioGroup>
+                </FormControl>
+            </CardContent>
+        </Card>
     </Layout>
   )
 }

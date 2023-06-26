60159bcdb12fd29685e5b6ed8e718d3bafdeb691
lagunasdaniel001 <lagunasdaniel001@gmail.com>
2023-06-26 17:27:20 -0500

    Leer las cookies

--- pages/theme-changer.tsx
+++ pages/theme-changer.tsx
@@ -1,9 +1,12 @@
-import { Layout } from "@/components/layout"
+import { ChangeEvent, FC, useEffect, useState } from "react"
+import { GetServerSideProps } from 'next'
 import {Card, CardHeader, CardContent, FormControl, FormLabel, RadioGroup, FormControlLabel, Radio} from '@mui/material'
-import { ChangeEvent, useEffect, useState } from "react"
 import Cookies from 'js-cookie'
 
-const ThemeChangerPage = () => {
+import { Layout } from "@/components/layout"
+
+const ThemeChangerPage:FC = (props) => {
+    console.log(props)
     const [currentTheme, setCurrentTheme] = useState('light')
     const onThemeChange = ( event:ChangeEvent<HTMLInputElement>) => {
         setCurrentTheme( event.target.value)
@@ -36,5 +39,20 @@ const ThemeChangerPage = () => {
     </Layout>
   )
 }
+
+// You should use getServerSideProps when:
+// - Only if you need to pre-render a page whose data must be fetched at request time
+
+export const getServerSideProps: GetServerSideProps = async ({req}) => {
+    const { name='No name' , theme='light'} = req.cookies;
+   
+    return {
+        props: {
+            name,
+            theme
+        }
+    }
+}
+
 
 export default ThemeChangerPage;
